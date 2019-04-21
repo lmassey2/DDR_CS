f60327de0a4f3d878fe0d3e6ac99fa75eefb9c84
lmassey2 <32546347+lmassey2@users.noreply.github.com>
2019-04-20 20:37:35 -0400

    Fixed user input

--- Pi_Control.c
+++ Pi_Control.c
@@ -71,7 +71,14 @@ int main(){
 	///// GET USER/SEND DATA LOOP /////
 	char x = 0;
     while(1){
-        x = getch();
+        /* use system call to make terminal send all keystrokes directly to stdin */
+		system ("/bin/stty raw");
+		while((x=getchar())!= '.') {
+			/* type a period to break out of the loop, since CTRL-D won't work raw */
+			putchar(x);
+		}
+		/* use system call to set terminal behaviour to more normal behaviour */
+		system ("/bin/stty cooked");
 
         switch(x)					// similar to video games, W/S will put the speed of the robot up and down
         {                           // A/D will turn the robot left and right
