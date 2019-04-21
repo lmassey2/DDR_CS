070b042beb8133771f26442b172c3943cd365509
lmassey2 <lmassey2.gmu.edu>
2019-04-21 18:49:48 +0100

    updated pi

--- Pi_Control.c
+++ Pi_Control.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <conio.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -14,10 +13,8 @@
 
 #define MIN_V 0
 #define MAX_V 100
-#define MIN_THETA -180
-#define MAX_THETA 180
-
-#pragma pack(0)  // turn off struct packing
+#define MIN_THETA 0
+#define MAX_THETA 360
 
 
 // structure containing speed and angle of robot
@@ -29,12 +26,13 @@ struct Velocity {
 
 // initialize global structure for speed and angle to 0
 volatile struct Velocity a;
-a.v = 0;
-a.theta = 0;
 
 
 int main(){
 	
+
+	a.v = 0;
+	a.theta = 0;
 	///// NETWORKING /////
 	int sockfd, portno, n;
     int serverlen;
@@ -43,8 +41,9 @@ int main(){
     char *hostname;
     int buf[BUFSIZE];
 	
-	hostname = "";  // make sure hostname and password match with uC access point
-    portno = "";
+	hostname = "192.168.1.1";  // make sure hostname and password match with uC access point
+    portno = "5000";
+	printf("hostname: %s    portnum: %s\n", hostname, portno);
 	
 	/* socket: create the socket */
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -70,34 +69,43 @@ int main(){
 	
 	///// GET USER/SEND DATA LOOP /////
 	char x = 0;
+	int time_to_send = 0;
     while(1){
-        /* use system call to make terminal send all keystrokes directly to stdin */
-		system ("/bin/stty raw");
-		while((x=getchar())!= '.') {
-			/* type a period to break out of the loop, since CTRL-D won't work raw */
-			putchar(x);
-		}
-		/* use system call to set terminal behaviour to more normal behaviour */
-		system ("/bin/stty cooked");
+        scanf("%c", &x);
+	printf("x = %c\n", x);
 
         switch(x)					// similar to video games, W/S will put the speed of the robot up and down
         {                           // A/D will turn the robot left and right
             case 'W':               // each input changes the global desired values of the robot, meaning 
             case 'w':               // only holding down W won't necessarily make the robot move forware, but
                 a.v = a.v + 5;      // after several presses of the W key the robot will move at a constant speed
+		time_to_send = 1;
                 break;
+	    case 'E':
+	    case 'e':
+		a.v = a.v - 5;
+		time_to_send = 1;
+		break;
             case 'A':
             case 'a':
-                a.theta + 5;
+                a.theta = 0;  // north
+		time_to_send = 1;
                 break;
             case 's':
             case 'S':
-                a.v = a.v - 5;
+                a.theta = 270; // east
+		time_to_send = 1;
                 break;
             case 'D':
             case 'd':
-                a.theta - 5;
+                a.theta = 180; // south
+		time_to_send = 1;
                 break;
+	    case 'F':
+	    case 'f':
+		a.theta = 90;  // west
+		time_to_send = 1;
+		break;
 
         }
 		if (MIN_V > a.v){
@@ -112,6 +120,8 @@ int main(){
 		if (MAX_THETA < a.theta){
 			a.theta = MAX_THETA;
 		}
+		printf("a.v = %d\n", a.v);
+		printf("a.theta = %d\n", a.theta);
 		
 		// now send the struct over UDP
 		buf[0] = a.v;
@@ -136,4 +146,4 @@ int main(){
 // 		a.theta = theta;
 //		Send(a);			put speed and angle into a struct and send over UDP
 // }						|||can also use netcat to make sure data is being sent|||
-}
+
